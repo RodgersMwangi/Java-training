d409b7f1c11278714b6a416150941616eb851d52
rowlrowl <rdgrsmwangi@gmail.com>
2026-01-19 11:38:41 +0300

    Multiplication table

--- src/main/java/io/github/javatraining/loops.java
+++ src/main/java/io/github/javatraining/loops.java
@@ -5,13 +5,15 @@ import java.util.Scanner;
 public class loops {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a positive number: ");
-        int number = scanner.nextInt();
-        while(number<=0){
-            System.out.println("Invalid, enter positive number");
-            number=scanner.nextInt();
+        System.out.println("Enter a number");
+        int number=scanner.nextInt();
+        System.out.println("Multiplication Table for "+ number);
+        while(number<0){
+            System.out.println("Please enter a positive number");
         }
-    System.out.println("Thank you, you have entered positive number");
+        for(int i=0; i<=10; i++){
+                System.out.println(number+ " * "+i +" = " + number*i);
+            }
     }
 
 }
